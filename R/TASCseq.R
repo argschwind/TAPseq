7e1a1663ba176654332f06efabc3cbd647255dfe
Andreas Gschwind <andreas.gschwind@stanford.edu>
2019-08-09 23:42:29 +0200

    Add package core components
    Add TASCseq.R containing package documentation and imports. Add onLoad.R containing code to look for required tool installations (currently only Primer3). Add documentation files and update NAMESPACE.

---
+++ R/TASCseq.R
@@ -0,0 +1,18 @@
+#' TASCseq: R-package to design primers for TASC-seq
+#'
+#' This package provides functions to select transcript isoforms and design PCR primers for
+#' TASC-seq.
+#'
+#' @section Installation:
+#' In order to use the full functionality, Primer3 needs to be installed and added to PATH.
+#' Furthermore, the \code{primer3_config} directory containing important config files should be
+#' located in the same directory as the \code{primer3_core} executable. If this is not practical,
+#' all functions interacting woth Primer3 have arguments to specify the path to these files.
+#'
+#' For more information on installation see: \url{https://github.com/argschwind/TASCseq}.
+#'
+#' @docType package
+#' @name TASCseq
+#'
+#' @import methods
+NULL
---
+++ R/onLoad.R
@@ -0,0 +1,76 @@
+## Check that all required 3rd party programs are installed
+
+#' Check if required tools are installed
+#'
+#' Check if requried software is installed and return paths to executables (if found).
+#'
+#' @return A \code{list} containing paths to required tools, if found, else NA.
+#' @keywords internal
+check_tool_installation <- function() {
+
+  tools <- list()
+
+  # get paths to required executables
+  tools[["primer3_core"]] <- Sys.which("primer3_core")
+
+  # NA if a tool is not found
+  tools[tools == ""] <- NA
+
+  # return list with path to tools
+  return(tools)
+
+}
+
+#' Actions to perform on loading
+#'
+#' Check for required tools and set tool paths as package options.
+#'
+#' @keywords internal
+.onLoad <- function(libname, pkgname) {
+
+  # check for required tools
+  tools <- check_tool_installation()
+
+  # raise warning if some tools are not found, else print message with paths to used tools
+  na_tools <- names(tools[is.na(tools)])
+  if (length(na_tools) > 0) {
+
+    warning("Following required software is not installed or not in PATH:",
+            "\n\n\t", paste0(na_tools, collapse = "\n\t"),
+            "\n\nPlease install these tools before trying to use this package!",
+            call. = FALSE)
+
+  }else{
+
+    # create strings combining tool name and path to tool
+    tools_print <- lapply(1:length(tools), FUN = function(x) {
+      paste(names(tools)[x], tools[[x]] , sep = ": ")
+    })
+
+    message("\nUsing the following tools:\n",
+            paste(unlist(tools_print), collapse = "\n"),
+            "\n")
+
+  }
+
+  # create tool paths for package options
+  tool_opts <- tools
+  names(tool_opts) <- paste0("primer3.", names(tool_opts))
+
+  ### TO DO: implement better! ---------------------------------------------------------------------
+  # add default path to primer3 thermodynamic parameters
+  therm_params <- paste0(dirname(tools[["primer3_core"]]), "/primer3_config/")
+  tool_opts[["primer3.thermodynamic_params_path"]] <- therm_params
+  ### ----------------------------------------------------------------------------------------------
+
+  # create package options
+  op <- options()
+  op.primer3 <- tool_opts
+
+  # set options
+  toset <- !(names(op.primer3) %in% names(op))
+  if(any(toset)) options(op.primer3[toset])
+
+  invisible()
+
+}
