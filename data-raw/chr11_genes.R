9d44e2fe4778e68307df22b174ff22649abc64dd
Andreas Gschwind <andreas.gschwind@stanford.edu>
2019-08-15 21:31:22 +0200

    Remove FANCF gene from example data
    Remove FANCF because it leads to a Primer3 hang-up when designing primers. Re-create all example data.

--- data-raw/chr11_genes.R
+++ data-raw/chr11_genes.R
@@ -20,13 +20,13 @@ chr11_genes <- annot_region[annot_region$type == "exon" &
 
 # expressed genes (to be removed once gene selection functions are available)
 expr_genes <- c("AKIP1", "ARFIP2", "BET1L", "C11orf58", "CARS", "CD151", "CD81", "CHID1", "COPB1",
-  "CSNK2A3", "CTR9", "CTSD", "DEAF1", "EIF3F", "EIF4G2", "FANCF", "FAR1", "GTF2H1", "HRAS",
-  "HTATIP2", "IFITM1", "IFITM2", "ILK", "IPO7", "LDHA", "MOB2", "MRPL17", "MRPL23", "NAP1L4",
-  "NCR3LG1", "NUCB2", "NUP98", "PDE3B", "PGAP2", "PHRF1", "PKP3", "PNPLA2", "POLR2L", "PRMT3",
-  "PSMA1", "PSMD13", "RASSF7", "RHOG", "RIC8A", "RNH1", "RPL27A", "RPLP2", "RPS13", "RRAS2", "RRM1",
-  "RRP8", "SAAL1", "SIGIRR", "SIRT3", "SLC25A22", "SPTY2D1", "STIM1", "SVIP", "TAF10", "TALDO1",
-  "TEAD1", "TIMM10B", "TMEM41B", "TMEM9B", "TOLLIP", "TPP1", "TRIM5", "TSG101", "TSPAN4", "TSSC4",
-  "USP47", "WEE1", "ZBED5", "ZNF143", "ZNF195", "ZNF215", "HBB", "HBD", "HBE1", "HBG1", "HBG2")
+  "CSNK2A3", "CTR9", "CTSD", "DEAF1", "EIF3F", "EIF4G2", "FAR1", "GTF2H1", "HRAS", "HTATIP2",
+  "IFITM1", "IFITM2", "ILK", "IPO7", "LDHA", "MOB2", "MRPL17", "MRPL23", "NAP1L4", "NCR3LG1",
+  "NUCB2", "NUP98", "PDE3B", "PGAP2", "PHRF1", "PKP3", "PNPLA2", "POLR2L", "PRMT3", "PSMA1",
+  "PSMD13", "RASSF7", "RHOG", "RIC8A", "RNH1", "RPL27A", "RPLP2", "RPS13", "RRAS2", "RRM1", "RRP8",
+  "SAAL1", "SIGIRR", "SIRT3", "SLC25A22", "SPTY2D1", "STIM1", "SVIP", "TAF10", "TALDO1", "TEAD1",
+  "TIMM10B", "TMEM41B", "TMEM9B", "TOLLIP", "TPP1", "TRIM5", "TSG101", "TSPAN4", "TSSC4", "USP47",
+  "WEE1", "ZBED5", "ZNF143", "ZNF195", "ZNF215", "HBB", "HBD", "HBE1", "HBG1", "HBG2")
 
 # only select exons of expressed genes
 chr11_genes <- chr11_genes[chr11_genes$gene_name %in% expr_genes]
