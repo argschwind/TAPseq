aff4998d518eb4e148170201c48a82c74ab4ebeb
Andreas Gschwind <andreas.gschwind@stanford.edu>
2024-02-01 10:25:34 -0800

    Fix bug in inferPolyASites() function
    Fix bug where an error was raised if polyA sites were found for a gene but placed outside of the terminal exon. The function now returns no polyA sites for these cases instead of an error. Also implement warning if polyA sites migh be placed outside of terminal exons based on provided user parameters.

--- R/polyA_from_dropseq.R
+++ R/polyA_from_dropseq.R
@@ -9,7 +9,9 @@
 #'   annotations of genes for which polyA sites are to be estimated.
 #' @param bam Path to .bam file containing aligned reads used for polyA site estimation.
 #' @param polyA_downstream (numeric) How far downstream of a peak in coverage are polyA sites
-#'   expected? Somewhat depends on input DNA fragment size. (default: 100).
+#'   expected? Somewhat depends on input DNA fragment size. (default: 100). Importantly, this value
+#'   should not be larger than half of the window size (wdsize), else polyA sites might be moved
+#'   outside of the transcripts, even if they were extended using the extend_downstream parameter.
 #' @param min_cvrg (numeric) Minimal coverage for peaks to be considered for polyA site estimation
 #'   (default: 0).
 #' @param wdsize (numeric) Window size to estimate sequencing coverage along transcripts (default:
@@ -47,50 +49,61 @@
 #' @export
 inferPolyASites <- function(genes, bam, polyA_downstream = 100, min_cvrg = 0, wdsize = 200,
                             by = 1, extend_downstream = 0, perc_threshold = 0.9, parallel = FALSE) {
-
+  
   # verify that genes is a GRangesList
   if (!is(genes , "GRangesList")) {
     stop("genes needs to be a GRangesList containing annotations per gene!", call. = FALSE)
   }
-
+  
+  #  raise warning if polyA_downstream is half or less of wdsize
+  if (polyA_downstream > wdsize / 2) {
+    warning("'polyA_downstream' is larger than 'wdsize' / 2. ",
+            "PolyA sites might be moved outside transcripts and missed.", call. = FALSE)
+  }
+  
   # calculate per base coverage for all genes ------------------------------------------------------
-
+  
   ## TO DO: load bam only for chromosomes of target genes
-
+  
+  message("Loading reads and computing coverage...")
+  
   # load all reads in bam file
   reads <- GenomicAlignments::readGAlignments(bam)
-
+  
   # calculate coverage per base
   read_cvrg <- GenomicAlignments::coverage(reads)
-
+  
   # find putative polyA sites ----------------------------------------------------------------------
-
+  
+  message("Inferring polyA sites...")
+  
   # get polyA sites for all genes
   if (parallel == TRUE) {
-
+    
     polyA_sites <- BiocParallel::bplapply(X = genes, FUN = polyA_site_gene, coverage = read_cvrg,
-                          polyA_downstream = polyA_downstream, wdsize = wdsize, by = by,
-                          extend_downstream = extend_downstream, perc_threshold = perc_threshold)
+                                          polyA_downstream = polyA_downstream, wdsize = wdsize, by = by,
+                                          extend_downstream = extend_downstream, perc_threshold = perc_threshold)
   } else {
-
+    
     polyA_sites <- lapply(X = genes, FUN = polyA_site_gene, coverage = read_cvrg,
                           polyA_downstream = polyA_downstream, wdsize = wdsize, by = by,
                           extend_downstream = extend_downstream, perc_threshold = perc_threshold)
-
+    
   }
-
+  
   # transform output to GRanges object
   polyA_sites <- unlist(GRangesList(polyA_sites))
-
+  
   # filter polyA sites according to min_cvrg
   polyA_sites <- polyA_sites[polyA_sites$score >= min_cvrg]
-
+  
   # sort according to position
   polyA_sites <- sort(polyA_sites)
-
+  
   # return output
+  message("Done!")
   return(polyA_sites)
-
+  
 }
 
 # helper functions =================================================================================
@@ -98,168 +111,168 @@ inferPolyASites <- function(genes, bam, polyA_downstream = 100, min_cvrg = 0, wd
 # infer polyA sites for one gene
 polyA_site_gene <- function(gene, coverage, polyA_downstream, wdsize, by, extend_downstream,
                             perc_threshold) {
-
-  # process annotation and coverage data -----------------------------------------------------------
-
+  
   # get chromosome and strand of gene
   chr <- as.character(unique(seqnames(gene)))
   strand <- as.character(unique(strand(gene)))
-
+  
   # merge exons of gene
   exons <- reduce(gene)
-
-  # find putative polyA site if gene is on the '+' strand
+  
+  # find putative polyA sites if gene is on the '+' strand -----------------------------------------
   if (strand == "+") {
-
+    
     # extend terminal exon downstream
     end(exons[length(exons)]) <- end(exons[length(exons)]) + extend_downstream
-
+    
     # exon ends and starts in spliced (concatenated) transcript
     exon_ends <- cumsum(width(exons))
     exon_starts <- c(1, exon_ends[-length(exon_ends)] + 1)
     tx_exons <- IRanges(exon_starts, exon_ends)
-
+    
     # calculate coverage in sliding windows along gene ---------------------------------------------
-
+    
     # coverage of gene
     gene_cvrg <- Views(coverage[[chr]], ranges(exons))
-
+    
     # coverage of spliced transcript (concatenated)
     tx_cvrg <- unlist(gene_cvrg)
-
+    
     # define windows based on strand of gene, so that the 3' end is for sure
     # covered by a window
     wd_ends <- seq(from = sum(width(exons)), to = wdsize, by = -by)
     wd_starts <- wd_ends - wdsize + 1
     wds <- IRanges(wd_starts, wd_ends)
-
+    
     # calculate coverage in each window
     wd_cvrg <- sum(Views(tx_cvrg, wds))
-
+    
     # define putative polyA site based on local maxima (peaks) -------------------------------------
-
+    
     # calculate local maxima in smoothed coverage
     peaks <- local_maxima(wd_cvrg)
     peaks_cvrg <- wd_cvrg[peaks]
-
+    
     # retain peaks which coverage is >= perc_threshold
     min_cvrg <- as.numeric(stats::quantile(wd_cvrg, probs = perc_threshold))
     peaks <- peaks[peaks_cvrg >= min_cvrg]
-
+    
     # get windows for these peaks
     peak_wds <- wds[peaks]
-
+    
     # get center of peaks
     peak_centers <- round(start(peak_wds) + wdsize / 2 - 1)
-
+    
     # define putative polyA site
     polyA_sites <- peak_centers + polyA_downstream
-
+    
     # if gene is on '-' strand
   } else if (strand == "-") {
-
+    
     start(exons[1]) <- start(exons[1]) - extend_downstream
-
+    
     exon_ends <- cumsum(width(exons))
     exon_starts <- c(1, exon_ends[-length(exon_ends)] + 1)
     tx_exons <- IRanges(exon_starts, exon_ends)
-
+    
     # calculate coverage in sliding windows along gene ---------------------------------------------
-
+    
     gene_cvrg <- Views(coverage[[chr]], ranges(exons))
-
+    
     tx_cvrg <- unlist(gene_cvrg)
-
+    
     wd_starts <- seq(from = 1, to = (sum(width(exons)) - wdsize + 1), by = by)
     wd_ends <- wd_starts + wdsize - 1
     wds <-IRanges(wd_starts, wd_ends)
-
+    
     wd_cvrg <- sum(Views(tx_cvrg, wds))
-
+    
     # define putative polyA site based on local maxima (peaks) -------------------------------------
-
+    
     peaks <- local_maxima(wd_cvrg)
     peaks_cvrg <- wd_cvrg[peaks]
-
+    
     min_cvrg <- as.numeric(stats::quantile(wd_cvrg, probs = perc_threshold))
     peaks <- peaks[peaks_cvrg >= min_cvrg]
-
+    
     peak_wds <- wds[peaks]
-
+    
     peak_centers <- round(start(peak_wds) + wdsize / 2 - 1)
-
+    
     polyA_sites <- peak_centers - polyA_downstream + 1
-
+    
   } else {
-
+    
     stop("Strand not '+' or '-' for at least 1 gene!", call. = FALSE)
-
+    
   }
-
-  # translate polyA sites to genomic coordinates
+  
+  # translate polyA sites to genomic coordinates ---------------------------------------------------
+  
+  # transform to IRanges object
+  polyA_sites <- IRanges(start = polyA_sites, end = polyA_sites)
+  
+  # add metadata
+  cov_bp <- wd_cvrg[peaks] / wdsize # coverage per bp of peaks
+  mcols(polyA_sites) <- data.frame("name" = rep_len("polyA_site", length(cov_bp)),
+                                   "score" = cov_bp)
+  
+  # sort and remove possible duplicate polyA sites due to rounding of coordinates
+  polyA_sites <- unique(sort(polyA_sites))
+  
+  # get exon overlapping with polyA sites
+  polyA_overlaps <- findOverlaps(query = tx_exons, subject = polyA_sites)
+  polyA_exons <- S4Vectors::queryHits(polyA_overlaps)
+  
+  # only retain polyA sites that overlap any exons
+  polyA_sites <- polyA_sites[S4Vectors::subjectHits(polyA_overlaps)]
+  
+  # convert any polyA sites overlapping transcripts to genomic coordinates
   if (length(polyA_sites) > 0) {
-
-    # transform to IRanges object
-    polyA_sites <- IRanges(start = polyA_sites, end = polyA_sites)
-
-    # add metadata
-    cov_bp <- wd_cvrg[peaks] / wdsize # coverage per bp of peaks
-    mcols(polyA_sites) <- data.frame("name" = "polyA_site",
-                                     "score" = cov_bp)
-
-    # sort and remove possible duplicate polyA sites due to rounding of coordinates
-    polyA_sites <- unique(sort(polyA_sites))
-
-    # get exon overlapping with polyA sites
-    polyA_overlaps <- findOverlaps(query = tx_exons, subject = polyA_sites)
-    polyA_exons <- S4Vectors::queryHits(polyA_overlaps)
-
-    # only retain polyA sites that overlap any exons
-    polyA_sites <- polyA_sites[S4Vectors::subjectHits(polyA_overlaps)]
-
+  
     # calculate distance of polyA site to the start of the overlapping exon
     pos_diff <- start(polyA_sites) - start(tx_exons[polyA_exons])
-
+    
     # translate polyA site coordinates to genomic coordinates
     polyA_start <- start(exons[polyA_exons]) + pos_diff
-
+    
     # create GRanges object with genomic coordinates of polyA site
     polyA_coords <- GRanges(seqnames = chr, strand = strand,
                             ranges = IRanges(polyA_start, polyA_start),
                             mcols(polyA_sites))
-
+    
   } else {
-
-    # if no polyA sites are found, return empty GRanges object
+    
+    # if no polyA sites overlap transcripts, return empty GRanges object
     polyA_coords <- GRanges()
-
+    
   }
-
-  # return output
+  
+  # return polyA sites with genomic coordinates
   return(polyA_coords)
-
+  
 }
 
 ## FUN: find local maxima in an ordered data series (after post in:
 #  https://stackoverflow.com/questions/6836409/finding-local-maxima-and-minima)
 local_maxima <- function(x) {
-
+  
   # Use -Inf instead if x is numeric (non-integer)
   y <- diff(c(-1, x)) > 0L
   # y <- diff(c(-.Machine$integer.max, x)) > 0L
-
+  
   rle(y)$lengths
-
+  
   y <- cumsum(rle(y)$lengths)
   y <- y[seq.int(1L, length(y), 2L)]
-
+  
   if (x[[1]] == x[[2]]) {
-
+    
     y <- y[-1]
-
+    
   }
-
+  
   # return vector with indices of local maxima
   return(y)
-
+  
 }
